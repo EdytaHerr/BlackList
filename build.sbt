c5ef00faba4343c6ce0e6eb40fa7b1144775c79c
eherczynska <edyta.herczynska@atos.net>
2018-01-26 09:04:10 +0100

    Empty project created

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "BlackList"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+        
---
+++ src/main/scala/DatabaseConnector.scala
@@ -0,0 +1,5 @@
+object DatabaseConnector {
+  def main(args: Array[String]): Unit = {
+  println("HelloWorld")
+  }
+}
